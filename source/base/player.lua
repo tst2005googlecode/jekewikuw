f47b47ed56094d7893f0304ea829b0cf76d09e85
Dagmar1177 <Dagmar1177@localhost>
2013-09-06 18:56:36 -0500

    Fiddling with movement in gameplay doesn't exactly work

--- source/base/player.lua
+++ source/base/player.lua
@@ -9,6 +9,12 @@ Player = {}
 Player.x = 0
 Player.y = 0
 
+key_act = {}
+key_act.up = false
+key_act.right = false
+key_act.down = false
+key_act.left = false
+
 function Player:new(x, y)
   newPlayer = {}
   newPlayer.x = x
@@ -19,21 +25,42 @@ function Player:new(x, y)
 end
 
 function Player:draw()
-  love.graphics.print("[-]", self.x, self.y)
-end
-
-function Player:update(dt)
- -- TODO
+  love.graphics.circle("fill", self.x, self.y, 10, 15)
 end
 
 function Player:keypressed(key)
-  if key == "up" then
-    self.y = self.y - 10
-  elseif key == "down" then
-    self.y = self.y + 10
-  elseif key == "left" then
-    self.x = self.x - 10
-  elseif key == "right" then
-    self.x = self.x + 10
+	if key == "up" then
+		key_act.up = true
+	elseif key == "down" then
+		key_act.down = true
+	elseif key == "left" then
+		key_act.right = true
+	elseif key == "right" then
+		key_act.right = true
   end
 end
+
+function Player:keyreleased(key)
+	if key == "up" then
+		key_act.up = false
+	elseif key == "right" then
+		key_act.right = false
+	elseif key == "down" then
+		key_act.down = false
+	elseif key == "left" then
+		key_act.left = false
+	end
+end
+
+function Player:update(dt)
+	if key_act.up then
+		self.y = self.y - 10
+	elseif key_act.down then
+		self.y = self.y + 10
+	elseif key_act.left then
+		self.x = self.x - 10
+	elseif key_act.right then
+		self.x = self.x + 10
+	end
+end
+
